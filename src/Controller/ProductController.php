996de179e78794c9768296f0a7f5af118abb3566
saifkader <seifeddine.abdelkader@esprit.tn>
2022-02-22 10:13:27 +0100

    Crud

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,21 +1,25 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Images;
 use App\Entity\Product;
-use App\Form\ProductType;
+use App\Form\Product1Type;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+
+
 
 /**
  * @Route("/product")
  */
 class ProductController extends AbstractController
 {
+     
     /**
      * @Route("/", name="product_index", methods={"GET"})
      */
@@ -32,10 +36,31 @@ class ProductController extends AbstractController
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $product = new Product();
-        $form = $this->createForm(ProductType::class, $product);
+        $form = $this->createForm(Product1Type::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            
+            $images = $form->get('images')->getData();
+
+            
+            foreach($images as $image){
+                
+                $fichier = md5(uniqid()) . '.' . $image->guessExtension();
+
+                
+                $image->move(
+                    $this->getParameter('images_directory'),
+                    $fichier
+                );
+
+                
+                $img = new Images();
+                $img->setName($fichier);
+                $product->addImage($img);
+
+            }
+
             $entityManager->persist($product);
             $entityManager->flush();
 
@@ -63,20 +88,43 @@ class ProductController extends AbstractController
      */
     public function edit(Request $request, Product $product, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(ProductType::class, $product);
+        $form = $this->createForm(Product1Type::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+            
+            
+             $images = $form->get('images')->getData();
 
-            return $this->redirectToRoute('product_index', [], Response::HTTP_SEE_OTHER);
+            
+             foreach($images as $image){
+                
+                 $fichier = md5(uniqid()) . '.' . $image->guessExtension();
+ 
+                
+                 $image->move(
+                     $this->getParameter('images_directory'),
+                     $fichier
+                 );
+ 
+                 
+                 $img = new Images();
+                 $img->setName($fichier);
+                 $product->addImage($img);
+             }
+ 
+             $this->getDoctrine()->getManager()->flush();
+ 
+             return $this->redirectToRoute('product_index', [], Response::HTTP_SEE_OTHER);
         }
+            
 
         return $this->render('product/edit.html.twig', [
             'product' => $product,
             'form' => $form->createView(),
         ]);
     }
+    
 
     /**
      * @Route("/{id}", name="product_delete", methods={"POST"})
@@ -90,4 +138,53 @@ class ProductController extends AbstractController
 
         return $this->redirectToRoute('product_index', [], Response::HTTP_SEE_OTHER);
     }
+    
+   /**
+     * @Route("/delete/image/{id}", name="product_delete_image", methods={"DELETE"})
+     */
+    public function deleteImage(Images $image, Request $request ){
+        
+        $data = json_decode($request->getContent(), true);
+
+      
+        if($this->isCsrfTokenValid('delete'.$image->getId(), $data['_token'])){
+            
+            $nom = $image->getName();
+            
+            unlink($this->getParameter('images_directory').'/'.$nom);
+
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($image);
+            $em->flush();
+
+            
+            return new JsonResponse(['success' => 1]);
+        }else{
+            return new JsonResponse(['error' => 'Token Invalide'], 400);
+            
+        }
+    }
+   /**
+     * @Route("/front/products", name="product_front_index", methods={"GET"})
+     */
+    public function indexFront(ProductRepository $productRepository): Response
+    {
+        return $this->render('product/indexFront.html.twig', [
+            'products' => $productRepository->findAll(),
+            
+        ]);
+    }
+    /**
+     * @Route("/front/products/{id}", name="product_front_show", methods={"GET"})
+     */
+    public function showFront(Product $product): Response
+    {
+        return $this->render('product/showFront.html.twig', [
+            'product' => $product,
+        ]);
+    }
+    
 }
+
+
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -3,13 +3,35 @@
 namespace App\Entity;
 
 use App\Repository\ProductRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Type;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints as Assert;
 /**
  * @ORM\Entity(repositoryClass=ProductRepository::class)
  */
 class Product
 {
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata): void
+    {
+        $metadata->addPropertyConstraint('name', new NotBlank());
+        $metadata->addPropertyConstraint('price', new NotBlank());
+        $metadata->addPropertyConstraint('price', new Assert\Positive());
+        $metadata->addPropertyConstraint('price', new Assert\GreaterThan(0));
+        $metadata->addPropertyConstraint('quantity', new NotBlank());
+        $metadata->addPropertyConstraint('quantity', new Assert\Positive());
+        $metadata->addPropertyConstraint('quantity', new Assert\GreaterThan(0));
+        $metadata->addPropertyConstraint('description', new NotBlank());
+
+
+     
+
+    }
+    
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -23,29 +45,42 @@ class Product
     private $name;
 
     /**
-     * @ORM\Column(type="float", nullable=true)
+     * @ORM\Column(type="float")
      */
     private $price;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $categorie;
+    private $description;
 
     /**
-     * @ORM\Column(type="integer", nullable=true)
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="products")
+     * @ORM\JoinColumn(nullable=false)
      */
-    private $quantite;
+    private $category;
 
     /**
-     * @ORM\ManyToOne(targetEntity=Categories::class, inversedBy="categorie")
+     * @ORM\Column(type="integer")
      */
-    private $categories;
+    private $quantity;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Images::class, mappedBy="productImg", orphanRemoval=true, cascade={"persist"})
+     */
+    private $images;
+
+    public function __construct()
+    {
+        $this->images = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
         return $this->id;
     }
+
+    
 
     public function getName(): ?string
     {
@@ -64,46 +99,77 @@ class Product
         return $this->price;
     }
 
-    public function setPrice(?float $price): self
+    public function setPrice(float $price): self
     {
         $this->price = $price;
 
         return $this;
     }
 
-    public function getCategorie(): ?string
+    public function getDescription(): ?string
     {
-        return $this->categorie;
+        return $this->description;
     }
 
-    public function setCategorie(string $categorie): self
+    public function setDescription(string $description): self
     {
-        $this->categorie = $categorie;
+        $this->description = $description;
 
         return $this;
     }
 
-    public function getQuantite(): ?int
+    public function getCategory(): ?Category
     {
-        return $this->quantite;
+        return $this->category;
     }
 
-    public function setQuantite(?int $quantite): self
+    public function setCategory(?Category $category): self
     {
-        $this->quantite = $quantite;
+        $this->category = $category;
 
         return $this;
     }
 
-    public function getCategories(): ?Categories
+    public function getQuantity(): ?int
     {
-        return $this->categories;
+        return $this->quantity;
     }
 
-    public function setCategories(?Categories $categories): self
+    public function setQuantity(int $quantity): self
     {
-        $this->categories = $categories;
+        $this->quantity = $quantity;
 
         return $this;
     }
+
+    /**
+     * @return Collection<int, Images>
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Images $image): self
+    {
+        if (!$this->images->contains($image)) {
+            $this->images[] = $image;
+            $image->setProductImg($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Images $image): self
+    {
+        if ($this->images->removeElement($image)) {
+            // set the owning side to null (unless already changed)
+            if ($image->getProductImg() === $this) {
+                $image->setProductImg(null);
+            }
+        }
+
+        return $this;
+    }
+    
 }
